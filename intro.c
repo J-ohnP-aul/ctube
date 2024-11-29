c0bc13da78268f5da22233809ccf908f6a5547b5
J-ohn P_aul <johnpaulnoblemen@gmail.com>
2024-11-30 00:18:31 +0300

    first time commit

---
+++ intro.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+//#include<stdbool.h> to work with boolean
+#include<math.h>
+int main(){
+    // printf("Ilove coding \n");
+    // printf("I am disciplene\n");
+    // printf("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9");
+
+    // int x; //declaration of var
+    // x = 12307; //initialization
+    // int y = 321; //declaration and initialization
+
+    // int age = 19; //integer
+    // float weight = 66.7; //float
+    // char grade = 'B'; //character
+    // char name[] = "Noble programmers"; //to emulate strings many char in an array
+    // double pi = 3.14233535366; //more acurate than floats fomat S %lf
+    // bool dcpln = false; //import stdbool otuput 1 true 0 false 
+    // char f = 100; //int -127 - 127 oupt  decimal int char(using asciitable)
+    // unsigned char C = 255; //0-255 %d or %c
+
+    // // printf("\n hello %s",name);
+    // // printf("\n your avarage grade is %c",grade);
+    // // printf("\n you  are %i  years old",age);
+    // // printf("\n last your wieght was %f maintain discipline in the gym eighty is the target",weight);
+    // // printf("\n %d",dcpln);
+
+    // short int h = 32767; // 2bytes -32768 - +ve 32768
+    // unsigned short int H = 65535; //2btye 
+    
+    // printf("%d",h);
+    // printf("%d",H);
+
+    // float item1 = 5.75;
+    // float item2 = 10.06;
+    // float item3 = 100.99;
+
+    // printf("item1: $%-8.2f\n", item1);
+    // printf("item1: $%-8.2f\n", item2);
+    // printf("item1: $%8.2f\n", item3);//decimal specifier and space bafter %
+
+    //constant
+    // const float PI = 3.1459;
+    // printf("%.3f",PI);
+
+    // int x = 5;
+    // int y = 2;
+
+    // float z = x / (float)y; //to convert an extng dttype
+
+    // printf("%.2f",z);
+    // char name[25];
+    // printf("whats ur name pal ");
+    // //scanf("%s", &name); can not read white space
+    // fgets(name, 25, stdin);//var name inpt size , stdin
+
+    // printf("hello %s \n", name);
+
+
+    // int adm;
+    // printf("we n mzee uko na iaka  how ? \n");
+    // scanf("%d", &adm);
+
+    // printf("you are: %d years old", adm);
+
+
+    // double A = sqrt(9);
+    // double B = pow(2, 4);//power no, p
+    // int R = round(3.144);//rounding off
+    // int D = ceil(3.144);//round a number up
+    // int E = floor(3.99);//round down
+    // double F = fabs(-100);//finds  absolute valuo
+    // //cos, log ,tan
+
+    // printf("%f\n",A);
+    // printf("%f\n",B);
+    // printf("%d\n",R);
+
+    return 0;
+}
